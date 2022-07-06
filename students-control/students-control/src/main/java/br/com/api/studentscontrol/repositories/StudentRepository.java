e532131de0b57b1d01d22e231c98a09b21a907e6
Gabriel Ferreira <gabrielferreira1d2015@gmail.com>
2022-07-06 14:20:52 -0300

    Create StudentRepository.java

---
+++ students-control/students-control/src/main/java/br/com/api/studentscontrol/repositories/StudentRepository.java
@@ -0,0 +1,11 @@
+package br.com.api.studentscontrol.repositories;
+
+import br.com.api.studentscontrol.models.StudentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentModel, UUID> {
+}
