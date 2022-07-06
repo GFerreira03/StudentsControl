b5b2d99a3f7bfdefc9061381cbbd2b9c3aef2413
Gabriel Ferreira <gabrielferreira1d2015@gmail.com>
2022-07-06 13:07:08 -0300

    basic configs

---
+++ students-control/students-control/src/main/java/br/com/api/studentscontrol/StudentsControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.studentscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsControlApplication.class, args);
+	}
+
+}
---
+++ students-control/students-control/src/test/java/br/com/api/studentscontrol/StudentsControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.studentscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
